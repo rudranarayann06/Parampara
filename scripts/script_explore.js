caad02b134f4a0a8ac6d06f987574ed9ce9b92f5
Rudra Narayan <rudranahak310@gmail.com>
2026-09-21 01:15:31 +0530

    Connect frontend to deployed backend

--- scripts/script_explore.js
+++ scripts/script_explore.js
@@ -5,7 +5,7 @@
    LIVE ARCHIVE
 ========================================================= */
 
-const API_BASE = "https://parampara-backend.onrender.com";
+const API_BASE = "https://parampara-backend-8yt9.onrender.com/";
 
 let archiveRecords = [];
 
